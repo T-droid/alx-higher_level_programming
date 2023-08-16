5910fbc2f9a1a2bf93fde9f1d56365fac1c6c910
Emmanuel Tindi <emmanueltindi66@gmail.com>
2023-08-17 00:46:23 +0300

    file update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+
     roman_numerals = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
     }
@@ -18,4 +21,4 @@ def roman_to_int(roman_string):
 
         prev_value = value
 
-    return total
+    return int(total)
