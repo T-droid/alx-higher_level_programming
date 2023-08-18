ee7cd7982a65fae1ff67810c5ae8678fc0e4d27e
Emmanuel Tindi <emmanueltindi66@gmail.com>
2023-08-18 12:36:37 +0300

    file update

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-    new_dict = {key: val for key, val in a_dictionary.items() if val != value}
-    return new_dict
+    while value in a_dictionary.values():
+        for k, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[k]
+                break
+
+    return (a_dictionary)
