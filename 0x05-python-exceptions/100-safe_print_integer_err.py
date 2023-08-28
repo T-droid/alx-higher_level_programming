9ed3bb7b3eb9c453210efc71a56f46072f4fe417
Emmanuel Tindi <emmanueltindi66@gmail.com>
2023-08-28 22:06:16 +0300

    update file

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,11 @@
 
 import sys
 
+
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
         return True
-    except ValueError as e:
-        print("Exception:", e, file=sys.stderr)
+    except (ValueError, TypeError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,11 +5,11 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(0, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
-        except (TypeError, ValueError):
-            print("Wrong type")
+        except TypeError:
+            print("wrong type")
             result = 0
         except ZeroDivisionError:
-            print("Division by 0")
+            print("division by 0")
             result = 0
         except IndexError:
             print("out of range")
