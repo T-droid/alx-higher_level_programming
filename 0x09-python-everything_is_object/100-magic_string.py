fbb516dc4a504911213821fd5a6d7081b11c83f1
Emmanuel Tindi <emmanueltindi66@gmail.com>
2023-09-05 18:32:01 +0300

    file update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-count = 0
 def magic_string():
-    global count; count += 1; return "BestSchool" + (", " + "BestSchool") * count
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ("BestSchool, " * (magic_string.count - 1) + "BestSchool")
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Defines the LockedClass class."""
+
 
 class LockedClass:
-    def __setattr__(self, name, value):
-        if name != "first_name":
-            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
-        super().__setattr__(name, value)
+    """
+    Prevents the user from dynamically creating new instance attributes.
+    """
+    __slots__ = ["first_name"]
