81e755026371e9441a65da1992e19d585ad43546
emmanuel tindi <emmanueltindi66@gmail.com>
2023-11-13 21:39:11 +0300

    file update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,11 @@
+#!/usr/bin/node
 
+const args = process.argv.slice(2);
+
+if (args.length === 0 || args.length === 1)
+	console.log(0);
+else
+{
+	const intArgs = args.sort((a, b) => a - b);
+	console.log(intArgs[intArgs.length - 2]);
+}
